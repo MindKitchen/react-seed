619e6325c89d7941e5af40bb6e60e46b2e2d40b8
Randall A. Gordon <randall@randallagordon.com>
2014-10-03 14:22:46 -0700

    Add sourcemap goodness

--- Gulpfile.js
+++ Gulpfile.js
@@ -9,13 +9,16 @@ var del = require("del");
 var mkdirp = require("mkdirp");
 var source = require("vinyl-source-stream");
 var sass = require("gulp-sass");
+var sourcemaps = require("gulp-sourcemaps");
+var uglify = require("gulp-uglify");
 var react = require("gulp-react");
 var jshint = require("gulp-jshint");
 var jsxcs = require("gulp-jsxcs");
 var lab = require("gulp-lab");
+var plumber = require("gulp-plumber");
 var browserSync = require("browser-sync");
 var reload = browserSync.reload;
- 
+
 var paths = {
   css: ["src/css/**/*.scss"],
   js: ["src/js/**/*.jsx"],
@@ -52,7 +55,7 @@ gulp.task("jscs", function() {
   gulp.src(paths.js)
     .pipe(jsxcs());
 });
- 
+
 gulp.task("lint", function() {
   gulp.src(paths.js)
     .pipe(jshint(".jshintrc"))
@@ -64,24 +67,30 @@ gulp.task("clean-css", function(done) {
   del([paths.build + "/css/*"], done);
   mkdirp(paths.build + "/css");
 });
- 
+
 gulp.task("css", ["clean-css"], function() {
   return gulp.src(paths.css)
+    .pipe(sourcemaps.init())
     .pipe(sass())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.build + "/css"))
     .pipe(reload({stream:true}));
 });
- 
+
 gulp.task("clean-js", function(done) {
   del([paths.build + "/js/*"], done);
   mkdirp(paths.build + "/js");
 });
- 
+
 gulp.task("js", ["clean-js"], function() {
-  browserify(paths.entrypoint)
+  browserify({
+    entries: paths.entrypoint,
+    debug: true
+  })
     .transform(reactify)
     .bundle()
     .pipe(source("js/bundle.js"))
+    //.pipe(uglify())
     .pipe(gulp.dest(paths.build))
     .pipe(reload({stream:true}));
 });
@@ -89,13 +98,13 @@ gulp.task("js", ["clean-js"], function() {
 gulp.task("clean-copy", function(done) {
   del([paths.build + "/" + paths.index], done);
 });
- 
+
 gulp.task("copy", ["clean-copy"], function () {
   gulp.src(paths.index)
     .pipe(gulp.dest(paths.build))
     .pipe(reload({stream:true}));
 });
- 
+
 gulp.task("watch", function() {
   gulp.watch(paths.css, ["css"]);
   gulp.watch(paths.js, ["js"]);
@@ -103,5 +112,5 @@ gulp.task("watch", function() {
 });
 
 gulp.task("build", ["jscs", "lint", "css", "js", "copy"]);
- 
+
 gulp.task("default", ["watch", "css", "js", "copy"]);
